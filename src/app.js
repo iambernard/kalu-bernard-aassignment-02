3302752bc1fcd41c03893024cac0ec85b7c8d5cc
Kalu Bernard <63473712+iambernard@users.noreply.github.com>
2022-07-12 04:05:41 +0000

    check challenge 1

--- src/app.js
+++ src/app.js
@@ -5,6 +5,13 @@ const { sumOfNumbers, countEvenNumbers, celsiusToFahrenheit } = assignment;
 if (sumOfNumbers && typeof sumOfNumbers === 'function'){
     console.log(sumOfNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 }
+ const sumOfNumber = [1,2,3,4,5,6,7,8,9,10];
+let sum = 0;
+
+for (let i = 0; i < sumOfNumber.length; i++) {
+    sum += sumOfNumber[i];
+}
+console.log(sum);
 
 if (countEvenNumbers && typeof countEvenNumbers === 'function'){
     console.log(countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
